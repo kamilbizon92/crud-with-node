ab152bdbc03c4cec6d2d62011012ca0d89e771bc
kamil <kamil.bizon@protonmail.com>
2018-10-20 22:04:15 +0200

    Install pug template engine

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const path = require('path');
 
 // Init app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 // Routing
 app.get('/', (req, res) => {
-  res.send('!')
+  res.render('index', {
+    title: 'Hello'
+  });
 });
 
+app.get('/articles/add', (req, res) => {
+  res.render('add_article', {
+    title: 'Add article'
+  });
+});
+
+// Start server
 app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`)
+  console.log(`Listening on port: ${PORT}`);
 });
